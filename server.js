b79cc9d97b868f145622149ca6c74e66c5daeb0a
khayuen <khayuen@yahoo.com>
2013-10-21 07:22:58 +0000

    update server.js

--- server.js
+++ server.js
@@ -1,8 +1,36 @@
-var conf = require('conf')
+var nconf = require('nconf')
+
+nconf
+  .argv()
+  .env()
+  .file('config.json')
+  .defaults({
+    targetHost: 'http://hypebeast.com',
+    cacheExp: 60*30
+  });
 
 var express = require('express');
+var winston = require('winston');
+var winst = require('winston-papertrail');
 var http = require('http');
+var request = require('request');
+var dom = require('./lib/dom');
 var app = express();
+
+var host = nconf.get('targetHost');
+
+app.use(express.compress());
+app.use(express.logger({
+  stream: {
+    write: function(msg, encoding) {
+      winston.info(msg);
+    }
+  }
+}));
+
+app.get('/robots.txt', function(req, res) {
+  res.end('User-agent: *\nDisallow: /');
+});
 
 app.get('/', function(req, res) {
   res.type('application/json');
@@ -10,13 +38,31 @@ app.get('/', function(req, res) {
     name: 'node-hbapi',
     description: 'Unofficial Hypebeast API',
     version: "0,0,1",
-    projectUrl: "https://github.com/khayuen/node-hbapi/",
-    author: "khayuen"
+    projectUrl: "https://github.com/khayuenkam/node-hbapi/",
+    author: "khayuen <khayuen@gmail.com>",
     process: {
       version: process.versions,
       memoryUsage: process.memoryUsage()
     }
   });
+});
+
+var regex = /^\/(news|style|arts|design|music|entertainment|lifestyle|tech|editorial|hypebeast-videos)$/
+
+app.get(regex, function(req, res) {
+  var url = host + req.path;
+  console.log(url);
+  request(url, function(err, response, body) {
+    if (err) { return; }
+
+    dom.page(body, function(error, data) {
+      res.end();
+    });
+  });
+});
+
+app.get(/^\/(paper|magazine)$/, function(req, res) {
+  console.log(req.path);
 });
 
 http.createServer(app, function(req, res) {
