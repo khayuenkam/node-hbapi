9545a9d5b469c17e1c7a11fb239a3af1d3c7973d
khayuen <khayuen@yahoo.com>
2013-11-03 09:06:52 +0000

    add redis cache in server.js

--- server.js
+++ server.js
@@ -5,8 +5,9 @@ nconf
   .env()
   .file('config.json')
   .defaults({
+    //redisUrl: 'redis://USERNAME:PASSWORD@HOST:PORT
     targetHost: 'http://hypebeast.com',
-    cacheExp: 60*30
+    cacheExp: 60*15
   });
 
 var express = require('express');
@@ -15,9 +16,11 @@ var winst = require('winston-papertrail');
 var http = require('http');
 var request = require('request');
 var dom = require('./lib/dom');
+var Redis = require('./lib/redis');
 var app = express();
 
 var host = nconf.get('targetHost');
+var TTL = nconf.get('cacheExp');
 
 app.use(express.compress());
 app.use(express.logger({
@@ -27,6 +30,20 @@ app.use(express.logger({
     }
   }
 }));
+
+var redis = Redis({
+  url: nconf.get('redisUrl'),
+  onConnect: function() {
+    winston.info('Connected to redis server');
+  },
+  onError: function() {
+    winston.error(err.toString() ? err.toString() : err);
+  }
+});
+
+app.get('/favicon.ico', function(req, res) {
+  res.send(204);
+});
 
 app.get('/robots.txt', function(req, res) {
   res.end('User-agent: *\nDisallow: /');
@@ -52,40 +69,59 @@ var regex = /^\/(news|style|arts|design|music|entertainment|lifestyle|tech|edito
 app.get(regex, function(req, res) {
   var url = host + req.path;
 
-  request(url, function(err, response, body) {
-    if (err) { return; }
+  redis.get(req.path, function(err, result) {
+    if (result) {
+      res.jsonp(result);
+    } else {
+      request(url, function(err, response, body) {
+        if (err) { return; }
 
-    dom.page(body, function(error, data) {
-      res.end();
-    });
+        dom.page(body, function(error, data) {
+          redis.set(req.path, data, TTL);
+          res.jsonp(data);
+        });
+      });
+    }
   });
 });
 
 app.get(/^\/(paper|magazine)$/, function(req, res) {
   var url = host + req.path;
 
-  request(url, function(err, response, body) {
-    if (err) { return; }
+  redis.get(req.path, function(err, result) {
+    if (result) {
+      res.jsonp(result);
+    } else {
+      request(url, function(err, response, body) {
+        if (err) { return; }
 
-    dom.publication(body, function(error , data) {
-      res.end();
-    })
+        dom.publication(body, function(error , data) {
+          redis.set(req.path, data, 60*60*24);
+          res.jsonp(data);
+        })
+      });
+    }
   });
-
 });
 
 app.get(/^\/[0-9]{4}\/([1-9]|1[0-2])\/[0-9a-zA-Z-]+$/, function(req, res) {
   var url = host + req.path;
 
-  request(url, function(err, response, body) {
-    if (err) { return; }
+  redis.get(req.path, function(err, result) {
+    if (result) {
+      res.jsonp(result);
+    } else {
+      request(url, function(err, response, body) {
+        if (err) { return; }
 
-    dom.detail(body, function(error , data) {
-      res.end();
-    })
+        dom.detail(body, function(error , data) {
+          redis.set(req.path, data, TTL);
+          res.jsonp(data);
+        })
+      });
+    }
   });
 });
 
 http.createServer(app, function(req, res) {
-   console.log('start server'); 
 }).listen(4000);
